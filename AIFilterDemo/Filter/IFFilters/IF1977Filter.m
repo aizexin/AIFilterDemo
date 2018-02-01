ed8f517cfbe3bcba7e87ac16bb94eb3f2b26532f
aizexin <443792431@qq.com>
2018-02-01 15:40:47 +0800

    删除无用文件

--- AIFilterDemo/Filter/IFFilters/IF1977Filter.m
+++ AIFilterDemo/Filter/IFFilters/IF1977Filter.m
@@ -7,13 +7,32 @@
 //
 
 #import "IF1977Filter.h"
-#import "AIFragmentShaderConfigure.h"
-
+NSString *const kIF1977ShaderString = SHADER_STRING
+(
+ precision lowp float;
+ 
+ varying highp vec2 textureCoordinate;
+ 
+ uniform sampler2D inputImageTexture;
+ uniform sampler2D inputImageTexture2;
+ 
+ void main()
+ {
+     vec3 texel = texture2D(inputImageTexture, textureCoordinate).rgb;
+     
+     texel = vec3(
+                  texture2D(inputImageTexture2, vec2(texel.r, .16666)).r,
+                  texture2D(inputImageTexture2, vec2(texel.g, .5)).g,
+                  texture2D(inputImageTexture2, vec2(texel.b, .83333)).b);
+     
+     gl_FragColor = vec4(texel, 1.0);
+ }
+ );
 @implementation AIFilter1977
 
 - (id)init;
 {
-    if (!(self = [super initWithFragmentShaderFromString:[AIFragmentShaderConfigure get1997ShaderString]]))
+    if (!(self = [super initWithFragmentShaderFromString:kIF1977ShaderString]))
     {
         return nil;
     }
